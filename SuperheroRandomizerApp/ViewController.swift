9b1ee9be0e2b3d23e5c0ed54cb48a600aeffcc76
ukn0wme <ukn0wme.design@gmail.com>
2024-11-29 03:03:32 +0500

    Initial Commit

---
+++ SuperheroRandomizerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SuperheroRandomizerApp
+//
+//  Created by Daniyal Nurgazinov on 29.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SuperheroRandomizerAppTests/SuperheroRandomizerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SuperheroRandomizerAppTests.swift
+//  SuperheroRandomizerAppTests
+//
+//  Created by Daniyal Nurgazinov on 29.11.2024.
+//
+
+import Testing
+@testable import SuperheroRandomizerApp
+
+struct SuperheroRandomizerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
